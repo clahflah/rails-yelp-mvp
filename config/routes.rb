da373668356294ff1ef8a484cdf4893ef1be642f
Clayton Flaherty <claytonflaherty92@gmail.com>
2022-08-17 13:06:08 +0200

    All rake passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "restaurants", to: 'restaurants#index'
+  get "restaurants/new", to: 'restaurants#new'
+  post "restaurants", to: 'restaurants#create'
+  get "restaurants/:id", to: 'restaurants#show', as: :restaurant
+  get "restaurants/:restaurant_id/reviews/new", to: 'reviews#new'
+  post "restaurants/:restaurant_id/reviews", to: 'reviews#create', as: "restaurant_reviews"
 end
